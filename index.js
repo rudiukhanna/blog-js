da5c346eeeef69a3269a6994170d36a0dce54273
Rudiuk Hanna <melomanna@gmail.com>
2023-04-23 22:47:42 +0300

    Lesson_35

---
+++ index.js
@@ -0,0 +1,30 @@
+const API_URL = 'https://gorest.co.in/public/v2/users';
+
+const userList = document.getElementById('user-list');
+
+/*<div class="list-group">
+  <a href="#" class="list-group-item list-group-item-action">A simple default list group item</a>
+
+  <a href="#" class="list-group-item list-group-item-action list-group-item-primary">A simple primary list group item</a>
+  <a href="#" class="list-group-item list-group-item-action list-group-item-secondary">A simple secondary list group item</a>
+  <a href="#" class="list-group-item list-group-item-action list-group-item-success">A simple success list group item</a>
+  <a href="#" class="list-group-item list-group-item-action list-group-item-danger">A simple danger list group item</a>
+  <a href="#" class="list-group-item list-group-item-action list-group-item-warning">A simple warning list group item</a>
+  <a href="#" class="list-group-item list-group-item-action list-group-item-info">A simple info list group item</a>
+  <a href="#" class="list-group-item list-group-item-action list-group-item-light">A simple light list group item</a>
+  <a href="#" class="list-group-item list-group-item-action list-group-item-dark">A simple dark list group item</a>
+</div>
+*/
+
+
+
+
+
+
+function getUsers() {
+    return fetch(API_URL)
+    .then(response => response.json())
+    .then((data) => console.log(data))
+}
+
+getUsers();
