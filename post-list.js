27ea7b39ab8a7d43ce10c2939110dd1eb9613d3d
Rudiuk Hanna <melomanna@gmail.com>
2023-04-24 12:39:26 +0300

    3rd screenplay was updated

--- post-list.js
+++ post-list.js
@@ -11,12 +11,20 @@ const API_URL_Posts = 'https://gorest.co.in/public/v2/posts';
 const postsContainer = document.getElementById('posts-container');
 
 function createPost(post) {
+    const postContainer = document.createElement('div');
+   
     const postTitle  = document.createElement('a');
-    postTitle.classList.add('list-group', 'list-group-item', 'list-group-item-action');
+    postTitle.classList.add('list-group-item-action');
     postTitle.href = `post.html?id=${post.body}`;
     postTitle.innerText = `${post.title}`;
+    postTitle.style.fontWeight = 'bold';
+    postContainer.appendChild(postTitle);
 
-    return postTitle;
+    const postBody = document.createElement('p');
+    postBody.innerText = `${post.body}`;
+    postContainer.appendChild(postBody);
+
+    return postContainer;
 }
 
 function getPosts() {
